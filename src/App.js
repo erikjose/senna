75655d8deab0d7bedbc33f3f4bbdeb13784dd2c7
Erik José Silva <serikjose@gmail.com>
2020-05-14 19:45:34 -0300

    change title

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Reacttt</h1>
+        <h1>DevOps na veia!</h1>
       </header>
     </div>
   );
